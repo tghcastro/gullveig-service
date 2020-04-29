409f11b940d162f9fe77d5a912b3d172c4485386
Thiago de Castro <tghcastro@gmail.com>
2020-04-29 21:47:36 +0100

    created project

---
+++ src/test/java/com/tghcastro/gullveig/gullveigcompaniesservice/GullveigCompaniesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tghcastro.gullveig.gullveigcompaniesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GullveigCompaniesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
