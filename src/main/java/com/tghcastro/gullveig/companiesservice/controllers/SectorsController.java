7c5f516f613ee3c73b72a81717a346a02553913c
Thiago de Castro <tghcastro@gmail.com>
2020-05-01 18:08:41 +0100

    improved error handling

--- src/main/java/com/tghcastro/gullveig/companiesservice/controllers/SectorsController.java
+++ src/main/java/com/tghcastro/gullveig/companiesservice/controllers/SectorsController.java
@@ -1,20 +1,28 @@
 package com.tghcastro.gullveig.companiesservice.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.tghcastro.gullveig.companiesservice.exceptions.SectorNotFoundException;
-import com.tghcastro.gullveig.companiesservice.repositories.SectorsRepository;
 import com.tghcastro.gullveig.companiesservice.models.Sector;
+import com.tghcastro.gullveig.companiesservice.repositories.SectorsRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/companiessectors")
-public class SectorsController extends ResponseEntityExceptionHandler {
+public class SectorsController {
 
     @Autowired
     public SectorsRepository companiesSectorRepository;
@@ -38,16 +46,37 @@ public class SectorsController extends ResponseEntityExceptionHandler {
         return companiesSectorRepository.saveAndFlush(sector);
     }
 
-    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
+    public void delete(@PathVariable Long id) {
         companiesSectorRepository.deleteById(id);
     }
 
-    @RequestMapping(value="{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
     public Sector update(@PathVariable Long id, @RequestBody Sector sector) {
         Sector existentSector = companiesSectorRepository.getOne(id);
         BeanUtils.copyProperties(sector, existentSector, "id");
         return companiesSectorRepository.saveAndFlush(existentSector);
+    }
+
+    // TODO: Improve error handling
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) throws JsonProcessingException {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        String responseBody = mapper.writeValueAsString(errors);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(responseBody);
     }
 }
