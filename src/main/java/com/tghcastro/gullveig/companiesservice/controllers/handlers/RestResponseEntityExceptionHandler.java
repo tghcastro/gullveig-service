132baa22b3865b1d3e3171c69f0bdba9e223dc7f
Thiago de Castro <tghcastro@gmail.com>
2020-05-01 12:00:15 +0100

    removed unnecessary annotation

--- src/main/java/com/tghcastro/gullveig/companiesservice/controllers/handlers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/tghcastro/gullveig/companiesservice/controllers/handlers/RestResponseEntityExceptionHandler.java
@@ -4,12 +4,10 @@ import com.tghcastro.gullveig.companiesservice.exceptions.SectorNotFoundExceptio
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@Component
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value= { SectorNotFoundException.class })
