2d1e93eb1f9891eb0ff5a789615eca5b17cc4e88
Thiago Castro <thiago.castro@sky.uk>
2020-05-19 18:13:52 +0100

    minor refactoring

--- src/main/java/com/tghcastro/gullveig/companiesservice/exceptions/SectorNotFoundException.java
+++ src/main/java/com/tghcastro/gullveig/companiesservice/exceptions/SectorNotFoundException.java
@@ -3,9 +3,11 @@ package com.tghcastro.gullveig.companiesservice.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.text.MessageFormat;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class SectorNotFoundException extends RuntimeException {
     public SectorNotFoundException(Long id) {
-        super("Sector not found [id:" + id + "]");
+        super(MessageFormat.format("Sector not found [id:{0}]", id));
     }
 }
