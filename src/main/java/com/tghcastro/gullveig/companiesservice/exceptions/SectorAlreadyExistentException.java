ea2320cdd354b7904c9058b23285ca8d9cdec711
Thiago Castro <thiago.castro@sky.uk>
2020-05-19 18:10:37 +0100

    error message fix

--- src/main/java/com/tghcastro/gullveig/companiesservice/exceptions/SectorAlreadyExistentException.java
+++ src/main/java/com/tghcastro/gullveig/companiesservice/exceptions/SectorAlreadyExistentException.java
@@ -4,10 +4,12 @@ import com.tghcastro.gullveig.companiesservice.models.Sector;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.text.MessageFormat;
+
 @ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
 public class SectorAlreadyExistentException extends RuntimeException {
     public SectorAlreadyExistentException(Sector foundSector) {
-        super(String.format("Sector with name already exists [Id: {}][Name: {}]",
+        super(MessageFormat.format("Sector already exists [Id: {0}][Name: {1}]",
                 foundSector.getId(),
                 foundSector.getName()));
     }
