68f77a3ef2af7b431f658fc8d6315b49d45628b8
Thiago Castro <tghcastro@gmail.com>
2020-10-15 18:18:32 +0100

    fix extends class path

--- companies-service/src/main/java/com/tghcastro/gullveig/companies/service/infrastructure/repositories/jpa/SectorsJpaRepository.java
+++ companies-service/src/main/java/com/tghcastro/gullveig/companies/service/infrastructure/repositories/jpa/SectorsJpaRepository.java
@@ -1,12 +1,13 @@
 package com.tghcastro.gullveig.companies.service.infrastructure.repositories.jpa;
 
+import com.tghcastro.gullveig.companies.service.domain.interfaces.repositories.SectorsRepository;
 import com.tghcastro.gullveig.companies.service.domain.models.Sector;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface SectorsJpaRepository extends JpaRepository<Sector, Long>, com.tghcastro.gullveig.companies.service.domain.interfaces.repositories.SectorsRepository {
+public interface SectorsJpaRepository extends JpaRepository<Sector, Long>, SectorsRepository {
     List<Sector> findAllByOrderByNameAsc();
 
     Optional<Sector> getByName(String name);
